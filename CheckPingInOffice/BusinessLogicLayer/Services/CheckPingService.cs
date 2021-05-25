b0fc2a8b53699ba619df3f91be7a187ea7aacf6b
Игорь Талавыря <igortalavuria@gmail.com>
2021-05-25 09:58:02 +0300

    add file Yesterday.txt

--- CheckPingInOffice/BusinessLogicLayer/Services/CheckPingService.cs
+++ CheckPingInOffice/BusinessLogicLayer/Services/CheckPingService.cs
@@ -3,7 +3,9 @@ using BusinessLogicLayer.Models.Response;
 using BusinessLogicLayer.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.NetworkInformation;
+using System.Text;
 using System.Timers;
 
 namespace BusinessLogicLayer.Services
@@ -11,6 +13,8 @@ namespace BusinessLogicLayer.Services
     public class CheckPingService : ICheckPingService
     {
         private static bool isStart;
+        private static bool isRead;
+        private static readonly string path = "Yesterday.txt";
 
         private static List<AllVariablesModel> allVariablesModels = new List<AllVariablesModel>();
 
@@ -18,67 +22,45 @@ namespace BusinessLogicLayer.Services
         {
             PingResponseModel pingResponseModel = new PingResponseModel();
 
-
-            if (DateTime.Now.Hour == 8 && DateTime.Now.Minute == 0 && DateTime.Now.Second >= 0 && DateTime.Now.Second <= 2)
+            try
             {
-                foreach (AllVariablesModel allVariablesModel in allVariablesModels)
+                AllVariablesModel connect = allVariablesModels.Find(x => x.ipAddress == ip);
+
+                if (connect != null)
                 {
-                    if (allVariablesModel.nAllSendForDay != 0)
+
+                    if (connect.nAllSendForDay != 0)
                     {
-                        allVariablesModel.percentsYesterday = allVariablesModel.nTrueSendForDay * 100 / allVariablesModel.nAllSendForDay;
-                        allVariablesModel.percentsYesterday = Math.Round(allVariablesModel.percentsYesterday, 2);
-
-                        allVariablesModel.nAllSendYesterday = allVariablesModel.nAllSendForDay;
-                        allVariablesModel.nTrueSendYesterday = allVariablesModel.nTrueSendForDay;
-                        allVariablesModel.nFalseSendYesterday = allVariablesModel.nFalseSendForDay;
+                        connect.percentsForDay = connect.nTrueSendForDay * 100 / connect.nAllSendForDay;
+                        connect.percentsForDay = Math.Round(connect.percentsForDay, 2);
                     }
+
+                    if (connect.nAllSendLastHour != 0)
+                    {
+                        connect.percentsLastHour = connect.nTrueSendLastHour * 100 / connect.nAllSendLastHour;
+                        connect.percentsLastHour = Math.Round(connect.percentsLastHour, 2);
+                    }
+
+
+                    pingResponseModel.nAllSendLastHour = connect.nAllSendLastHour;
+                    pingResponseModel.nTrueSendLastHour = connect.nTrueSendLastHour;
+                    pingResponseModel.nFalseSendLastHour = connect.nFalseSendLastHour;
+                    pingResponseModel.percentsLastHour = connect.percentsLastHour;
+
+                    pingResponseModel.nAllSendForDay = connect.nAllSendForDay;
+                    pingResponseModel.nFalseSendForDay = connect.nFalseSendForDay;
+                    pingResponseModel.nTrueSendForDay = connect.nTrueSendForDay;
+                    pingResponseModel.percentsForDay = connect.percentsForDay;
+
+                    pingResponseModel.nAllSendYesterday = connect.nAllSendYesterday;
+                    pingResponseModel.nFalseSendYesterday = connect.nFalseSendYesterday;
+                    pingResponseModel.nTrueSendYesterday = connect.nTrueSendYesterday;
+                    pingResponseModel.percentsYesterday = connect.percentsYesterday;
                 }
             }
-
-            if (DateTime.Now.Hour == 8 && DateTime.Now.Minute == 0 && DateTime.Now.Second >= 4 && DateTime.Now.Second <= 6)
-            {
-                foreach (AllVariablesModel allVariablesModel in allVariablesModels)
-                {
-                    allVariablesModel.nAllSendForDay = 0;
-                    allVariablesModel.nTrueSendForDay = 0;
-                    allVariablesModel.nFalseSendForDay = 0;
-                }
-            }
-
-            AllVariablesModel connect = allVariablesModels.Find(x => x.ipAddress == ip);
-
-            if (connect != null)
+            catch
             {
 
-               
-
-                if (connect.nAllSendForDay != 0)
-                {
-                    connect.percentsForDay = connect.nTrueSendForDay * 100 / connect.nAllSendForDay;
-                    connect.percentsForDay = Math.Round(connect.percentsForDay, 2);
-                }
-
-                if (connect.nAllSendLastHour != 0)
-                {
-                    connect.percentsLastHour = connect.nTrueSendLastHour * 100 / connect.nAllSendLastHour;
-                    connect.percentsLastHour = Math.Round(connect.percentsLastHour, 2);
-                }
-
-
-                pingResponseModel.nAllSendLastHour = connect.nAllSendLastHour;
-                pingResponseModel.nTrueSendLastHour = connect.nTrueSendLastHour;
-                pingResponseModel.nFalseSendLastHour = connect.nFalseSendLastHour;
-                pingResponseModel.percentsLastHour = connect.percentsLastHour;
-
-                pingResponseModel.nAllSendForDay = connect.nAllSendForDay;
-                pingResponseModel.nFalseSendForDay = connect.nFalseSendForDay;
-                pingResponseModel.nTrueSendForDay = connect.nTrueSendForDay;
-                pingResponseModel.percentsForDay = connect.percentsForDay;
-
-                pingResponseModel.nAllSendYesterday = connect.nAllSendYesterday;
-                pingResponseModel.nFalseSendYesterday = connect.nFalseSendYesterday;
-                pingResponseModel.nTrueSendYesterday = connect.nTrueSendYesterday;
-                pingResponseModel.percentsYesterday = connect.percentsYesterday;
             }
 
             return pingResponseModel;
@@ -88,46 +70,125 @@ namespace BusinessLogicLayer.Services
         {
             IpAddressResponseModel  ipAddressResponseModels = new IpAddressResponseModel();
 
-            if (!isStart)
+            try
             {
-                isStart = true;
-
-                allVariablesModels.Add(new AllVariablesModel
+                if (!isStart)
                 {
-                    nameConnect = "Office",
-                    ipAddress = "10.15.8.1",
+                    isStart = true;
 
-                    nAllSendLastHour = 0,
-                    nTrueSendLastHour = 0,
-                    nFalseSendLastHour = 0,
-                    percentsLastHour = 0,
+                    allVariablesModels.Add(new AllVariablesModel
+                    {
+                        nameConnect = "Office",
+                        ipAddress = "10.15.8.1",
 
-                    nAllSendForDay = 0,
-                    nTrueSendForDay = 0,
-                    nFalseSendForDay = 0,
-                    percentsForDay = 0,
+                        nAllSendLastHour = 0,
+                        nTrueSendLastHour = 0,
+                        nFalseSendLastHour = 0,
+                        percentsLastHour = 0,
 
-                    nAllSendYesterday = 0,
-                    nTrueSendYesterday = 0,
-                    nFalseSendYesterday = 0,
-                    percentsYesterday = 0,
+                        nAllSendForDay = 0,
+                        nTrueSendForDay = 0,
+                        nFalseSendForDay = 0,
+                        percentsForDay = 0,
 
-                    timer = new Timer(1000)
-                });
+                        nAllSendYesterday = 0,
+                        nTrueSendYesterday = 0,
+                        nFalseSendYesterday = 0,
+                        percentsYesterday = 0,
 
-                AllVariablesModel allVariablesModel = allVariablesModels.Find(x => x.ipAddress == "10.15.8.1");
+                        timer = new Timer(1000)
+                    });
 
-                allVariablesModel.timer.Elapsed += (o, e) => getPing("10.15.8.1");
-                allVariablesModel.timer.AutoReset = true;
-                allVariablesModel.timer.Start();
+                    AllVariablesModel allVariablesModel = allVariablesModels.Find(x => x.ipAddress == "10.15.8.1");
+
+                    allVariablesModel.timer.Elapsed += (o, e) => getPing("10.15.8.1");
+                    allVariablesModel.timer.AutoReset = true;
+                    allVariablesModel.timer.Start();
+                }
+
+                foreach (AllVariablesModel ipAddress in allVariablesModels)
+                {
+                    ipAddressResponseModels.ipAddress.Add(new IpAddressModel { nameConnect = ipAddress.nameConnect, ipAddress = ipAddress.ipAddress });
+                    
+                    if(ipAddress.nAllSendYesterday == 0)
+                    {
+                        isRead = true;
+                    }
+                }
+
+                if (isRead)
+                {
+                    readInFile();
+                    isRead = false;
+                }
             }
 
-            foreach (AllVariablesModel ipAddress in allVariablesModels)
+            catch
             {
-                ipAddressResponseModels.ipAddress.Add( new IpAddressModel { nameConnect = ipAddress.nameConnect, ipAddress = ipAddress.ipAddress });
+
             }
 
             return ipAddressResponseModels;
+        }
+
+        private void readInFile()
+        {
+            try
+            {
+                if (File.Exists(path))
+                {
+                    var text = File.ReadAllLines(path, Encoding.UTF8);
+
+                    AllVariablesModel connect = new AllVariablesModel();
+
+                    foreach (string str in text)
+                    {
+                        if (str.Contains("ipAddress:"))
+                        {
+                            string ip = str.Substring(10);
+
+                            connect = allVariablesModels.Find(x => x.ipAddress == ip);
+
+                        }
+
+                        if (connect != null)
+                        {
+                            if (str.Contains("nAllSendYesterday:"))
+                            {
+                                double nAllSendYesterday = Double.Parse(str.Substring(18));
+
+                                connect.nAllSendYesterday = nAllSendYesterday;
+                            }
+
+                            if (str.Contains("nFalseSendYesterday:"))
+                            {
+                                double nFalseSendYesterday = Double.Parse(str.Substring(20));
+
+                                connect.nFalseSendYesterday = nFalseSendYesterday;
+                            }
+
+                            if (str.Contains("nTrueSendYesterday:"))
+                            {
+                                double nTrueSendYesterday = Double.Parse(str.Substring(19));
+
+                                connect.nTrueSendYesterday = nTrueSendYesterday;
+                            }
+
+                            if (str.Contains("percentsYesterday:"))
+                            {
+                                double percentsYesterday = Double.Parse(str.Substring(18));
+
+                                connect.percentsYesterday = percentsYesterday;
+                            }
+                        }
+                    }
+                }
+            }
+
+            catch
+            {
+
+            }
         }
 
         public IpResponseModel addIp(string name, string ip)
@@ -286,6 +347,37 @@ namespace BusinessLogicLayer.Services
             try
             {
                 AllVariablesModel connect = allVariablesModels.Find(x => x.ipAddress == ip);
+
+                if (DateTime.Now.Hour == 8 && DateTime.Now.Minute == 0 && DateTime.Now.Second == 0)
+                {
+                    if (File.Exists(path))
+                    {
+                        File.Delete(path);
+                    }
+                }
+
+                if (DateTime.Now.Hour == 8 && DateTime.Now.Minute == 0 && DateTime.Now.Second == 1)
+                {
+
+                    if (connect.nAllSendForDay != 0)
+                    {
+                        connect.percentsYesterday = connect.nTrueSendForDay * 100 / connect.nAllSendForDay;
+                        connect.percentsYesterday = Math.Round(connect.percentsYesterday, 2);
+
+                        connect.nAllSendYesterday = connect.nAllSendForDay;
+                        connect.nTrueSendYesterday = connect.nTrueSendForDay;
+                        connect.nFalseSendYesterday = connect.nFalseSendForDay;
+
+                    }
+     
+                    string text = $"****************************\nipAddress:{connect.ipAddress}\nnAllSendYesterday:{connect.nAllSendYesterday}\nnFalseSendYesterday:{connect.nFalseSendYesterday}\nnTrueSendYesterday:{connect.nTrueSendYesterday}\npercentsYesterday:{connect.percentsYesterday}\n";
+
+                    File.AppendAllText(path, text);
+                  
+                    connect.nAllSendForDay = 0;
+                    connect.nTrueSendForDay = 0;
+                    connect.nFalseSendForDay = 0;
+                }
 
                 Ping ping = new Ping();
 
